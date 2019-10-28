a69b28ce4f9fa5a68f438f96e386db2c9c0e88da
喻聪 <yucongcong@i-syp.com>
2019-10-28 15:21:38 +0800

    代码提交

--- rabbitmq-direct-delay-producer/src/main/java/com/yucong/RabbitConsts.java
+++ rabbitmq-direct-delay-producer/src/main/java/com/yucong/RabbitConsts.java
@@ -17,23 +17,4 @@ public class RabbitConsts {
     public static final String DELAY_EXCHANGE = "delay.exchange";
     
     
-    
-    
-    
-    
-/*    # 自定义配置。 配置交换器exchange、路由键routing-key、队列名称 queue name
-    # 交换器名称
-    mq.config.exchange=log.direct
-    # info级别queue的名称
-    mq.config.queue.info=log.info
-    # info级别的路由键
-    mq.config.queue.info.routing.key=log.info.routing.key
-
-    # 交换器名称
-    mq.config.delay.exchange=log.delay.direct
-    # info级别queue的名称
-    mq.config.delay.queue.info=log.delay.info
-    # info级别的路由键
-    mq.config.delay.queue.info.routing.key=log.delay.info.routing.key*/
-    
 }
